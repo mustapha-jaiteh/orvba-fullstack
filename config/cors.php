41e3031bbfbb46d81ca981478d76f57edb8aceb9
mustapha-jaiteh <jaitehm20@gmail.com>
2025-06-16 10:59:16 +0000

    commit-16-06-2025

--- config/cors.php
+++ config/cors.php
@@ -2,12 +2,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://breakdown-assistance.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
 
 
